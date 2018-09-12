0b76a90e06860102a5d8b2b25717658ec576a86d
隋国珍 <s15863549041@126.com>
2018-09-12 19:28:26 +0800

    修复bug

--- time-service/time-service-impl/src/main/generated/com/time/article/service/converter/business/permission/ResourceConverterImpl.java
+++ time-service/time-service-impl/src/main/generated/com/time/article/service/converter/business/permission/ResourceConverterImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-09-01T10:18:43+0800",
+    date = "2018-09-12T19:25:48+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_51 (Oracle Corporation)"
 )
 public class ResourceConverterImpl implements ResourceConverter {
