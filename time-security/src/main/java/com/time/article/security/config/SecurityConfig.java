89a6c053e1b3f55db21c04c65e7a3a5108b10193
隋国珍 <s15863549041@126.com>
2018-09-13 17:50:15 +0800

    spring security

---
+++ time-security/src/main/java/com/time/article/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.time.article.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author suiguozhen on 18/09/12
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * Override this method to configure the {@link HttpSecurity}. Typically subclasses
+     * should not invoke this method by calling super as it may override their
+     * configuration. The default configuration is:
+     *
+     * <pre>
+     * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+     * </pre>
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception if an error occurs
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and().authorizeRequests().anyRequest().authenticated();
+    }
+}
