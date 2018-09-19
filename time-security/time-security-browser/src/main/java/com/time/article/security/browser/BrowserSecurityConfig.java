6acfc7e844294601690b01bc92bc7caf68c8f782
隋国珍 <s15863549041@126.com>
2018-09-19 16:20:14 +0800

    统一规划gradle，抽取到父类

--- time-security/time-security-browser/src/main/java/com/time/article/security/browser/BrowserSecurityConfig.java
+++ time-security/time-security-browser/src/main/java/com/time/article/security/browser/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.time.article.security.browser;
 
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author suiguozhen on 18/09/19
  */
-public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
 }
--- time-service/time-service-impl/src/main/generated/com/time/article/service/converter/business/permission/ResourceConverterImpl.java
+++ time-service/time-service-impl/src/main/generated/com/time/article/service/converter/business/permission/ResourceConverterImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2018-09-19T10:37:22+0800",
+    date = "2018-09-19T15:58:08+0800",
     comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_51 (Oracle Corporation)"
 )
 public class ResourceConverterImpl implements ResourceConverter {
