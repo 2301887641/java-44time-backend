622a98e76e5de6395ca601295794e74545c55426
隋国珍 <s15863549041@126.com>
2018-11-14 19:39:36 +0800

    别写starter

---
+++ time-manager/time-manager-captcha-starter-autoconfigure/src/main/java/com/time/manager/captcha/CaptchaConfig.java
@@ -0,0 +1,14 @@
+package com.time.manager.captcha;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 验证码配置
+ * @author suiguozhen on 18/11/14
+ */
+@Configuration
+public class CaptchaConfig {
+
+
+
+}
--- time-manager/time-manager-captcha/src/main/java/com/time/article/manager/captcha/config/CaptchaConfig.java
+++
@@ -1,8 +0,0 @@
-package com.time.article.manager.captcha.config;
-
-/**
- * 验证码配置
- * @author suiguozhen on 18/11/14
- */
-public class CaptchaConfig {
-}
