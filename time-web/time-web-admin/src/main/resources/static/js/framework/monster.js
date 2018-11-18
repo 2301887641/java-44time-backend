5926282c7ad8d731b1b236543aa0da9099e6479e
隋国珍 <s15863549041@126.com>
2018-11-18 16:48:10 +0800

    新创建monster ui类构建tips

--- time-web/time-web-admin/src/main/resources/static/js/framework/monster.js
+++ time-web/time-web-admin/src/main/resources/static/js/framework/monster.js
@@ -30,15 +30,25 @@ monster.prototype = {
             },
             //构造
             build: function (message, key) {
-                let html = [],innerStyle="height:100%;",monsterStyle='box-shadow:0 0 4px rgba(0,0,0,.2);font-family:"Microsoft YaHei",SimSun;position:fixed;border-radius: 4px;height: 40px;color: #fff;padding:0 15px;background: #4c93ff;top:74px;left:50%;margin-left:-112px;opacity:1;display:block;z-index:95;';
+                let html = [];
                 !!this.type[key] ? html.push('<div class="monster-tips ' + this.type[key] + '">') :
-                    html.push('<div class="monster-tips" style="'+monsterStyle+'">');
+                    html.push('<div class="monster-tips" style="' + monsterStyle + '">');
                 html.push('<div class="tip-inner">');
                 html.push('<span class="tip-icon icon-tips-success"></span>');
                 html.push('<span class="tip-msg">' + message + '</span>');
                 html.push('</div></div>');
                 html = html.join("")
                 return html;
+            },
+            //给html添加style样式
+            htmlStyle: function () {
+                let monsterStyle = 'box-shadow:0 0 4px rgba(0,0,0,.2);font-family:"Microsoft YaHei",SimSun;position:fixed;border-radius: 4px;height: 40px;color: #fff;padding:0 15px;background: #4c93ff;top:74px;left:50%;margin-left:-112px;opacity:1;display:block;z-index:95;',
+                    innerStyle = "height:100%;",
+                    iconTips='display: inline-block;width: 22px;height: 45px;margin: 0 1px 0 0;padding-left: 1px;',
+                    iconSuccess='line-height: 41px;font-size: 13px;',
+
+
+
             }
         }
         let inners = new inner(message, type = "success");
